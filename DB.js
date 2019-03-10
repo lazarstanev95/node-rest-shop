1a1fdbd5ffe5c90e77a33fd786d6074d50b73d86
Lazar Stanev <ironman95@gmail.com>
2019-03-10 22:13:01 +0200

    first commit

---
+++ DB.js
@@ -0,0 +1,3 @@
+module.exports = {
+    DB: 'mongodb://localhost:27017/node-rest-shop'
+}
---
+++ api/routes/products.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+const multer = require('multer');
+const checkAuth = require('../middleware/check-auth');
+const ProductsController = require('../controllers/products');
+
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, './uploads/');
+    },
+    filename: function(req, file, cb) {
+        cb(null, new Date().toISOString().replace(/:/g, '-') + file.originalname);
+    }
+});
+
+const fileFilter = (req, file, cb) => {
+    if(file.mimetype === 'image/jpeg' || file.mimetype === 'image/png'){
+        cb(null, true)
+    } else {
+        cb(null, false)
+    }
+}
+const upload = multer({
+    storage: storage, 
+    limits: {
+        fileSize: 1024 * 1024 * 5
+    },
+    fileFilter: fileFilter
+});
+
+const Product = require('../models/product');
+
+router.get('/', ProductsController.products_get_all);
+
+router.post('/', checkAuth, upload.single('productImage'), ProductsController.products_create_product);
+
+//example 1
+/* router.get('/:productId', (req, res, next) => {
+    const id = req.params.productId;
+    if (id === 'special') {
+        res.status(200).json({
+            message: 'You discovered the special ID',
+            id: id
+        });
+    } else {
+        res.status(200).json({
+            message: 'You passed an ID'
+        });
+    }
+}); */
+
+//example 2
+router.get('/:productId', ProductsController.products_get_product);
+
+router.patch('/:productId', checkAuth, ProductsController.products_update_product);
+
+router.delete('/:productId', checkAuth, ProductsController.products_delete_product);
+
+module.exports = router;
